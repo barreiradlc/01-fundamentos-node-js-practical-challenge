96421bf0bc54de3b9e0caa68b118a323d3e1544c
Augusto Barreira <barreira266@hotmail.com>
2024-05-08 02:22:59 -0300

    Update module

--- prisma/migrations/20240508045149_/migration.sql
+++ prisma/migrations/20240508045149_/migration.sql
@@ -1,14 +1,14 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
     "completed_at" TIMESTAMP(3),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_title_key" ON "User"("title");
+CREATE UNIQUE INDEX "Task_title_key" ON "Task"("title");
